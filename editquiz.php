b80940fa7dcce08dcc70504edc05352a7f53fae5
abosmraa <samerfakih15@gmail.com>
2025-04-27 01:39:30 +0300

    add wditquiz API, check if user is logged in

---
+++ editquiz.php
@@ -0,0 +1,22 @@
+<?php
+// Return JSON responses
+header("Content-Type: application/json");
+// Start session
+session_start();
+
+// Include DB connection
+include 'connect.php';
+
+// Read JSON input
+$data = json_decode(file_get_contents("php://input"), true);
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode([
+        "success" => false,
+        "message" => "User not logged in"
+    ]);
+    exit;
+}
+
+
